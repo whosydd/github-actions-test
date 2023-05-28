44ce25aa3b2c79fe30ae40421950be586f9bb0eb
GY <dev.youngkwok718@gmail.com>
2023-05-28 15:57:20 +0800

    edit

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-console.log("hello world!");
-console.log("hello world!");
+// const res = [1, 2, 3, 4, 5].filter((item) => item === 1 || item === 3);
+// console.log("res:", res);
+
+const res = [1, 2, 3, 4, 5].filter((item) => item >= 1 && item < 3);
+console.log("res:", res);
